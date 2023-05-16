775b5cc355dcd118a359c10e661534d5c96d8140
Ilia Moiseev <villeman.5@yandex.ru>
2023-05-16 23:03:20 +0300

    Move settings into generate

--- generate.py
+++ generate.py
@@ -2,16 +2,27 @@ import os
 import sys
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(SCRIPT_DIR)
 sys.path.append(SCRIPT_DIR)
 
 from datagen import Generator
-from settings import (
-    source_folder,
-    destination_folder,
-    camera_angles_range,
-    camera_distance_range,
-    render_per_input,
-)
+
+
+# The folder with images to render
+source_folder = os.path.join(BASE_DIR, "src")
+
+# The folder to place renders
+destination_folder = os.path.join(BASE_DIR, "dst")
+
+# The angles and distance are the spherical coordinates of the camera
+# given that the position of the object in the scene is (0,0,0)
+# render_per_input times different angles and distances are sampled
+# from these ranges
+camera_angles_range = [[70, -60], [110, 60]]
+
+camera_distance_range = [3, 5]
+
+render_per_input = 2
 
 
 if __name__ == "__main__":
@@ -21,5 +32,4 @@ if __name__ == "__main__":
         camera_angles_range=camera_angles_range,
         camera_distance_range=camera_distance_range,
         render_per_input=render_per_input,
-    )
-    gen.run()
+    ).run()
