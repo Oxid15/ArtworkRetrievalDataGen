d764c76beb7fed10f1963a470f2d546f9c4bdf9d
Ilia Moiseev <villeman.5@yandex.ru>
2023-05-16 18:17:30 +0300

    Make settings into a simple script

--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
 # The folder with images to render on the wall
-source_folder: "C:\\Users\\oxid\\Desktop\\src"
+source_folder = "C:\\Users\\oxid\\Desktop\\src"
 
 # The folder to place renders
-destination_folder: "C:\\Users\\oxid\\Desktop\\dst"
+destination_folder = "C:\\Users\\oxid\\Desktop\\dst"
 
 # The angles and distance are the spherical coordinates of the camera
 # given that the position of the object in the scene is (0,0,0)
 # render_per_input times different angles and distances are sampled
 # from these ranges
-camera_angles_range: [[70, -60], [110, 60]]
+camera_angles_range = [[70, -60], [110, 60]]
 
-camera_distance_range: [3, 5]
+camera_distance_range = [3, 5]
 
-render_per_input: 3
+render_per_input = 3
